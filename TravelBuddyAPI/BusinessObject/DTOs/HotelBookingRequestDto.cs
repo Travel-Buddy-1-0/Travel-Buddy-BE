7d8cbb86b569c01b2e35be4a5d97b75a3357b5b5
HungHQ <142767137+MeoSuytGay@users.noreply.github.com>
2025-10-02 17:46:01 +0700

    update

--- TravelBuddyAPI/BusinessObject/DTOs/HotelBookingRequestDto.cs
+++ TravelBuddyAPI/BusinessObject/DTOs/HotelBookingRequestDto.cs
@@ -15,7 +15,7 @@ public class HotelBookingRequestDto
     public int Guests { get; set; }
     public int HotelId { get; set; }
     public int RoomId { get; set; }
-    public int RestaurantId { get; set; }
+    public int? RestaurantId { get; set; }
 }
 
 
